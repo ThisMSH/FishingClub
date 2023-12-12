93d64656fe2eb8afae1c5cee9369cf5bd6acc40d
El Mahdi S.H. <midoyugi@hotmail.com>
2023-12-12 17:42:14 +0100

    Creating all the endpoints in HuntingController

--- Back-end/src/main/java/com/fishingclub/main/controllers/HuntingController.java
+++ Back-end/src/main/java/com/fishingclub/main/controllers/HuntingController.java
@@ -1,4 +1,56 @@
 package com.fishingclub.main.controllers;
 
+import com.fishingclub.main.dto.HuntingDTO;
+import com.fishingclub.main.dto.noRelations.HuntingNoRelDTO;
+import com.fishingclub.main.services.HuntingService;
+import com.fishingclub.main.utils.ResponseHandler;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/hunting")
 public class HuntingController {
+    private final HuntingService huntingService;
+
+    @Autowired
+    public HuntingController(HuntingService huntingService) {
+        this.huntingService = huntingService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getHunting(@PathVariable Integer id) {
+        HuntingDTO hunting = huntingService.getOne(id);
+
+        return ResponseHandler.success(
+                "The hunting has been fetched successfully.",
+                HttpStatus.OK,
+                hunting
+        );
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> createHunting(@RequestBody @Valid HuntingNoRelDTO hunt) {
+        HuntingDTO hunting = huntingService.create(hunt);
+
+        return ResponseHandler.success(
+                "The hunting has been created successfully.",
+                HttpStatus.CREATED,
+                hunting
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteHunting(@PathVariable Integer id) {
+        HuntingDTO hunting = huntingService.delete(id);
+
+        return ResponseHandler.success(
+                "The hunting has been managed successfully.",
+                HttpStatus.OK,
+                hunting
+        );
+    }
 }
