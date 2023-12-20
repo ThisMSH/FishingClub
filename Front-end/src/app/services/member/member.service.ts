512e2387233b9597a69ef9a4b4d7fde21d35ebd0
El Mahdi S.H. <midoyugi@hotmail.com>
2023-12-20 18:23:12 +0100

    Finishing MemberService [FISH-42]

--- Front-end/src/app/services/member/member.service.ts
+++ Front-end/src/app/services/member/member.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import { MemberRequest } from 'src/app/models/member/member-request';
 import { MemberResponse } from 'src/app/models/member/member-response';
 import { PaginationResponse } from 'src/app/models/response/pagination-response';
+import { Response } from 'src/app/models/response/response';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
@@ -22,5 +24,27 @@ export class MemberService {
         return this.http.get<PaginationResponse<MemberResponse>>(
             `${this.url}?page=${page}&size=${size}&sortBy=${sortBy}&sortOrder=${sortOrder}&fullName=${fullName}`
         );
+    }
+
+    getMember(id: number): Observable<Response<MemberResponse>> {
+        return this.http.get<Response<MemberResponse>>(`${this.url}/${id}`);
+    }
+
+    createMember(member: MemberRequest): Observable<Response<MemberResponse>> {
+        return this.http.post<Response<MemberResponse>>(
+            `${this.url}/add`,
+            member
+        );
+    }
+
+    updateMember(member: MemberRequest): Observable<Response<MemberResponse>> {
+        return this.http.put<Response<MemberResponse>>(
+            `${this.url}/update`,
+            member
+        );
+    }
+
+    deleteMember(id: number): Observable<Response<MemberResponse>> {
+        return this.http.delete<Response<MemberResponse>>(`${this.url}/delete/${id}`);
     }
 }
